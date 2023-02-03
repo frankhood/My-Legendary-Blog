ab3a15092ed1b30ddc2b1bb5ea73212b810a97f0
fnetti90 <f.netti@frankhood.it>
2023-02-03 13:32:17 +0100

    branch develop per esercitazione

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,8 +1,5 @@
 
-from django.urls import path
-from blog.api import views as api_views
 
 urlpatterns = [
-    path("post/", api_views.PostListDetailAPIView.as_view(), name="post-list-api"),
-    path("post/<slug:slug>/", api_views.PostListDetailAPIView.as_view(), name="post-detail-api"),
+
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListTemplateView.as_view(), name='post_list'),
-    path('<slug:slug>/', views.PostDetailView.as_view(), name='post-detail'),
+
 ]
 
 
