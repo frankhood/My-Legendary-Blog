d89612b22a0ba476d3f29c375f16fc540efdd341
dvd.ria <d.ria@frankhood.it>
2023-02-13 11:25:18 +0100

    Fixed base projectr

--- myblog/settings.py
+++ myblog/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     'webpack_loader',
     
-    'blog',
+    # 'blog',
 ]
 
 MIDDLEWARE = [
@@ -124,8 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- myblog/urls.py
+++ myblog/urls.py
@@ -18,17 +18,17 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf.urls import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', include('blog.urls')),
-    path('api-auth/', include('rest_framework.urls'))
-
+    # path('', include('blog.urls')),
+    # path('api-auth/', include('rest_framework.urls'))
 ]
 
-urlpatterns += [
-    path("api/v1/", include("blog.api.urls")),
-]
+# urlpatterns += [
+#     path("api/v1/", include("blog.api.urls")),
+# ]
 
-urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
