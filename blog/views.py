81a1aa8cae194d9d6318969ad0739c89d7384877
fnetti90 <f.netti@frankhood.it>
2023-02-17 15:28:33 +0100

    add path api in view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from . import models
+from django.urls import reverse, reverse_lazy
 
 
 def post_list_view(request):
     context = {
-        "object_list": models.Post.objects.all()
+        "object_list": models.Post.objects.all(),
+        "post_list_api_url": request.build_absolute_uri(reverse_lazy("post-list-api"))
     }
     return render(request, "blog/post_list.html", context)
 
