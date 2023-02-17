f27e06c4f9aca931e2e77958347e405a7b611d6c
Maurizio Brandonisio <m.brandonisio@frankhood.it>
2023-02-17 19:18:17 +0100

    demo FE completa

--- fe-src/src/main.js
+++ fe-src/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './assets/css/common.css';
+import PostListCmp from './components/post-list-cmp.vue';
+import PostListAsyncCmp from './components/post-list-async-cmp.vue';
+import PostDetailCmp from './components/post-detail-cmp.vue';
 
-createApp(App).mount('#app')
+const app = createApp({});
+app.component('post-list-cmp', PostListCmp);
+app.component('post-list-async-cmp', PostListAsyncCmp);
+app.component('post-detail-cmp', PostDetailCmp);
+app.mount('#app');
